35f15e50ffff0ea780c2d2e20cf90251cea44e8b
Fatma <fmutlu3002@gmail.com>
2023-07-12 21:54:41 +0300

    Generic Repository Pattern applied

---
+++ src/Core/Portal.Application/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using Portal.Domain.Entities.Common;
+using System.Linq.Expressions;
+
+namespace Portal.Application.Repositories
+{
+    public interface IGenericRepository<T>:IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+        Task<T> GetByIdAysnc(string id);
+        Task<T> GetSingleAysnc(Expression<Func<T, bool>> method);
+        Task<bool> AddAysnc(T Model);
+        bool Update(T Model);
+        Task<int> SaveAysnc();
+        void Delete(T Model);
+    }
+}
---
+++ src/Infrastructure/Portal.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Portal.Application.Repositories;
+using Portal.Domain.Entities.Common;
+using Portal.Persistence.Context;
+using System.Linq.Expressions;
+
+namespace Portal.Persistence.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly PortalAPIDbContext _context;
+        public GenericRepository(PortalAPIDbContext context)
+        {
+            _context = context;
+        }
+        public DbSet<T> Table => _context.Set<T>();
+
+        public async Task<bool> AddAysnc(T Model)
+        {
+            EntityEntry entityEntry = await Table.AddAsync(Model);
+            return entityEntry.State == EntityState.Added;
+        }
+
+        public void Delete(T Model)
+        {
+            Table.Remove(Model);
+        }
+
+        public async Task<int> SaveAysnc()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public bool Update(T Model)
+        {
+            EntityEntry entityEntry = _context.Update(Model);
+            return entityEntry.State == EntityState.Modified;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            var query = Table.AsQueryable().AsNoTracking();
+            return query;
+        }
+
+        public async Task<T?> GetByIdAysnc(string id)
+        {
+            var query = Table.AsQueryable().AsNoTracking() ;
+            return await query.FirstOrDefaultAsync(data => data.Id == int.Parse(id));
+        }
+
+        public async Task<T?> GetSingleAysnc(Expression<Func<T, bool>> method)
+        {
+            var query = await Table.AsQueryable().AsNoTracking().FirstOrDefaultAsync(method);
+            return query;
+        }
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
+        {
+            var query = Table.Where(method).AsQueryable().AsNoTracking();
+            return query;
+        }
+
+    }
+}
