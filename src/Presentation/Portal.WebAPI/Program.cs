fdbe8ca4e6110f0dfa03e3a3f98af84e0c3d568a
Fatma <fmutlu3002@gmail.com>
2023-07-26 10:31:15 +0300

    customize serilog columns

--- src/Presentation/Portal.WebAPI/Program.cs
+++ src/Presentation/Portal.WebAPI/Program.cs
@@ -12,8 +12,11 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Text.Json;
 using Portal.Infrastructure;
-using Serilog;
 using Serilog.Core;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System.Collections.ObjectModel;
+using Portal.WebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -57,10 +60,35 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddPersistenceService();
 
+#region columnoptions
+SqlColumn sqlColumn = new SqlColumn();
+sqlColumn.ColumnName = "UserName";
+sqlColumn.DataType = System.Data.SqlDbType.NVarChar;
+sqlColumn.PropertyName = "UserName";
+sqlColumn.DataLength = 50;
+sqlColumn.AllowNull = true;
+ColumnOptions columnOpt = new ColumnOptions();
+columnOpt.Store.Remove(StandardColumn.Properties);
+columnOpt.Store.Add(StandardColumn.LogEvent);
+columnOpt.AdditionalColumns = new Collection<SqlColumn> { sqlColumn };
+#endregion
+
 Logger log = new LoggerConfiguration()
-    .WriteTo.Console()
+
     .WriteTo.File("logs/log.txt")
-    .WriteTo.MSSqlServer(builder.Configuration.GetConnectionString("MicrosoftSQL"), "logs", autoCreateSqlTable: true)
+    .WriteTo.MSSqlServer(
+    connectionString: builder.Configuration.GetConnectionString("MicrosoftSQL"),
+     sinkOptions: new MSSqlServerSinkOptions
+     {
+         AutoCreateSqlTable = true,
+         TableName = "logs",
+     },
+     appConfiguration: null,
+     columnOptions: columnOpt
+    )
+    .Enrich.FromLogContext()
+    .Enrich.With<CustomUserNameColumn>()
+    .MinimumLevel.Information()
     .CreateLogger();
 
 builder.Host.UseSerilog(log);
