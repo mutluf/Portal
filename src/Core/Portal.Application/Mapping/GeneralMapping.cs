45a4064790f1a4cd5be63dc7c28a2a12dd175a7d
Fatma <fmutlu3002@gmail.com>
2023-07-27 09:54:02 +0300

    survey mappings

--- src/Core/Portal.Application/Mapping/GeneralMapping.cs
+++ src/Core/Portal.Application/Mapping/GeneralMapping.cs
@@ -1,13 +1,16 @@
 ﻿using AutoMapper;
 using Portal.Application.DTOs;
+using Portal.Application.DTOs.SurveyDTOs;
 using Portal.Application.Features.Commands.Categories.CreateCategory;
 using Portal.Application.Features.Commands.Educations.CreateEducation;
 using Portal.Application.Features.Commands.Educations.UpdateEducation;
 using Portal.Application.Features.Commands.Seminars.CreateSeminar;
 using Portal.Application.Features.Commands.Seminars.UpdateSeminar;
 using Portal.Application.Features.Commands.Users.CreateUser;
+using Portal.Application.Features.Commands.Users.LoginUser;
 using Portal.Application.Features.Commands.Workshops.CreateWorkshop;
 using Portal.Domain.Entities;
+using Portal.Domain.Entities.Survey;
 using Portal.Domain.Entities.Users;
 
 namespace Portal.Application.Mapping
@@ -28,8 +31,43 @@ namespace Portal.Application.Mapping
             CreateMap<CreateCategoryRequest, Category>().ReverseMap();
 
             CreateMap<UpdateEducationRequest, Education>().ReverseMap(); 
-            CreateMap<UpdateSeminarRequest, Seminar>().ReverseMap(); 
-          
+            CreateMap<UpdateSeminarRequest, Seminar>().ReverseMap();
+
+
+            //survey mappings
+            //CreateMap<GetResultByIdResponse, Survey>().ReverseMap();
+
+            CreateMap<Survey, SurveyDTO>().ForMember(s => s.Questions, o => o.MapFrom(a => a.Questions)).ReverseMap();
+
+            CreateMap<Question, QuestionDTO>().ForMember(s => s.Options, o => o.MapFrom(a => a.Options)).ReverseMap();
+
+            CreateMap<Option, OptionDTO>().ReverseMap();
+
+
+            CreateMap<Response, ResponseDTO>().ForMember(s => s.Questions, o => o.MapFrom(a => a.Questions)).ReverseMap();
+            CreateMap<AnsweredQuestionDTO, AnsweredQuestion>().ForMember(s => s.Options, o => o.MapFrom(a => a.Options)).ReverseMap();
+            CreateMap<AnsweredOption, AnsweredOptionDTO>().ReverseMap();
+            CreateMap<Vote, VoteDTO>().ReverseMap();
+
+            CreateMap<AnsweredOption, AnsweredOptionDTO>().ReverseMap();
+
+            CreateMap<CreateUserRequest, User>().ReverseMap();
+            CreateMap<LoginUserRequest, User>().ReverseMap();
+
+            CreateMap<Survey, SurveyResponse>().ForMember(s => s.Questions, o => o.MapFrom(a => a.Questions)).ReverseMap();
+
+            CreateMap<Question, QuestionResponse>().ForMember(s => s.Options, o => o.MapFrom(a => a.Options)).ReverseMap();
+
+            CreateMap<Option, OptionResponse>().ForMember(s => s.AnsweredOptions, o => o.MapFrom(a => a.AnsweredOptions)).ReverseMap();
+
+
+
+            //CreateMap<Survey, GetResultByIdResponse>().ForMember(s => s.Questions, o => o.MapFrom(a => a.Questions)).ReverseMap();
+
+            CreateMap<Question, QuestionDTO>().ForMember(s => s.Options, o => o.MapFrom(a => a.Options)).ReverseMap();
+
+            CreateMap<Option, OptionDTO>().ReverseMap();
+
         }
     }
 }
