6e1c45794b3202093f999e27fc0f209b091b0ee1
Fatma <fmutlu3002@gmail.com>
2023-08-02 16:45:40 +0300

    Unit test for blog controller

---
+++ src/Test/UnitTest/PortalUnitTest/ControllersUnitTest/BlogsControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Portal.Application.Features.Commands.Blogs.CreateBlog;
+using Portal.Application.Features.Commands.Blogs.DeleteBlog;
+using Portal.Application.Features.Queries.Blogs.GetBlogById;
+using Portal.WebAPI.Controllers;
+
+namespace PortalUnitTest.ControllersUnitTest
+{
+    public class BlogsControllerTests
+    {
+        // Örnekler için sahte (Mock) IMediator
+        private readonly Mock<IMediator> _mediatorMock;
+
+        // BlogsController nesnesi
+        private readonly BlogsController _blogsController;
+
+        public BlogsControllerTests()
+        {
+            // Moq kullanarak sahte IMediator oluşturuluyor
+            _mediatorMock = new Mock<IMediator>();
+
+            // BlogsController'ı sahte IMediator ile oluştur
+            _blogsController = new BlogsController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateBlog_Should_Return_OkResult()
+        {
+            // Arrange
+            var request = new CreateBlogRequest { /* gerekli alanları doldur */ };
+
+            // Act
+            var result = await _blogsController.CreateBlog(request);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task Get_Should_Return_OkResult_With_Valid_Blog()
+        {
+            // Arrange
+            var blogId = 1;
+            var request = new GetBlogByIdRequest { Id = blogId };
+            var blog = new GetBlogByIdResponse { /* sahte blog verisi oluştur */ };
+
+            // Mediator'den dönen cevap ayarlanıyor
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetBlogByIdRequest>(), default)).ReturnsAsync(blog);
+
+            // Act
+            var result = await _blogsController.Get(request);
+
+            // Assert
+            var okObjectResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsType<GetBlogByIdResponse>(okObjectResult.Value);
+            Assert.Equal(blog, model);
+        }
+
+        [Fact]
+        public async Task Delete_Should_Return_OkResult()
+        {
+            // Arrange
+            var blogId = 1;
+            var request = new DeleteBlogRequest { Id = blogId };
+
+            // Act
+            var result = await _blogsController.Delete(request);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+    }
+}
+
---
+++ src/Test/UnitTest/PortalUnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PortalUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
