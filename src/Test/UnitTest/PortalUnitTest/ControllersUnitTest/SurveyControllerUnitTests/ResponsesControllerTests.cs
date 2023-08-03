c2bff8c9c3cb56a785c5b621ba701dd8c9d82221
Fatma <fmutlu3002@gmail.com>
2023-08-03 14:57:05 +0300

    Result controller tests

--- src/Test/UnitTest/PortalUnitTest/ControllersUnitTest/SurveyControllerUnitTests/ResponsesControllerTests.cs
+++ src/Test/UnitTest/PortalUnitTest/ControllersUnitTest/SurveyControllerUnitTests/ResponsesControllerTests.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Portal.API.Controllers;
+using Portal.Application.Features.SurveyFeatures.Commands.Responses.CreateResponse;
 
 namespace PortalUnitTest.ControllersUnitTest.SurveyControllerUnitTests
 {
-    internal class ResponsesControllerTests
+    public class ResponsesControllerTests
     {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ResponsesController _responsesController;
+
+        public ResponsesControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _responsesController = new ResponsesController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task Post_Should_Return_OkResult()
+        {
+            // Arrange
+            var request = new CreateResponseRequest
+            {
+                Response= new()
+                {
+                    SurveyId = 1,
+                    UserId = 1,
+                    Questions = null 
+                }
+            };
+
+            // Act
+            var result = await _responsesController.Post(request);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
     }
 }
---
+++ src/Test/UnitTest/PortalUnitTest/ControllersUnitTest/SurveyControllerUnitTests/ResultsControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Portal.API.Controllers;
+using Portal.Application.Features.SurveyFeatures.Queries.Results;
+
+namespace PortalUnitTest.ControllersUnitTest.SurveyControllerUnitTests
+{
+    public class ResultsControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ResultsController _resultsController;
+
+        public ResultsControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _resultsController = new ResultsController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task Get_Should_Return_OkResult_With_Valid_Result()
+        {
+            // Arrange
+            var resultId = 1;
+            var request = new GetResultByIdRequest { Id = resultId };
+            var response = new GetResultByIdResponse
+            {
+                Description = "Test",
+                OptionVoteCount = 1,
+                ResponseCount=1,
+                SolvedCount=1,
+                Id = resultId,
+                Questions= null,
+                Title= "Test",
+                UserId= 1,
+            };
+
+            _mediatorMock.Setup(m => m.Send(request, default)).ReturnsAsync(response);
+
+            // Act
+            var result = await _resultsController.Get(request);
+
+            // Assert
+            var okObjectResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsType<GetResultByIdResponse>(okObjectResult.Value);
+            Assert.Equal(response, model);
+        }
+    }
+}
