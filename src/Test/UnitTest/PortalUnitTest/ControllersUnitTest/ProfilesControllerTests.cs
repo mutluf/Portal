e0e8202b20b28e0a15ce7eb43e35f3116fa63eb2
Fatma <fmutlu3002@gmail.com>
2023-08-03 09:08:44 +0300

    UserProfile tests

---
+++ src/Test/UnitTest/PortalUnitTest/ControllersUnitTest/ProfilesControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Portal.Application.Features.Commands.Users.ChangeRole;
+using Portal.WebAPI.Controllers;
+
+namespace PortalUnitTest.ControllersUnitTest
+{
+    public class ProfilesControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ProfilesController _profilesController;
+
+        public ProfilesControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _profilesController = new ProfilesController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task ChangeRole_Should_Return_OkResult()
+        {
+            // Arrange
+            var request = new ChangeRoleRequest
+            {
+                Id = 1,
+                UserRole = "Producer"
+            };
+
+            // Act
+            var result = await _profilesController.ChangeRole(request);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+    }
+}
